0805001432c5236c6945234fdc5354675e6c028b
jeremynassar <jeremynassar@gmail.com>
2022-03-09 23:28:14 -0500

    DOM Model adding P

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let para1= document.getElementById("firstp");
 const para2 = document.getElementById("secondp");
 
-para1.innerHTML= "Finally working on JS"
+para1.innerHTML= "Finally working on JS";
 para2.innerHTML = "Second paragraph";
+
+para1.onclick = (event) => {
+
+ para1.style.color = "blue"; 
+}
+
+
+para2.onclick = (event)=>{
+    let para3 = document.createElement('p');
+    para3.id = "thirdp";
+    para3.innerHTML="This is our new paragraph";
+    document.body.appendChild(para3);
+}
